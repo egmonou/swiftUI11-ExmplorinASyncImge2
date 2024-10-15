f4ea13763a90ed49cf4a42980540ddea5eb2b8f3
monour <mohamednour25@gmail.com>
2024-10-15 10:05:25 +0400

    Initial Commit

---
+++ 11-ExmplorinASyncImge/_1_ExmplorinASyncImgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_ExmplorinASyncImgeApp.swift
+//  11-ExmplorinASyncImge
+//
+//  Created by Mohamed Nour on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct _1_ExmplorinASyncImgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
